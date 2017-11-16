1ede56cd70f14041545e138706fa586a71dc958b
amin <a.rashidbeigi@gmail.com>
2017-11-16 17:11:48 +0330

    cpu and memory usage added to multiThread for ubuntu

--- multiThread.cpp
+++ multiThread.cpp
@@ -2,15 +2,98 @@
 // Created by amin on 11/12/17.
 //
 
-#include <iostream>
 #include <thread>
+#include <iostream>
 #include <time.h>
+#include <unistd.h>
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
 
 #define SIZE 1000000
 
 using namespace std;
 
 int range;
+
+int parseLine(char* line){
+    int i = strlen(line);
+    const char* p = line;
+    while (*p <'0' || *p > '9') p++;
+    line[i-3] = '\0';
+    i = atoi(p);
+    return i;
+}
+
+int getValue(){
+    FILE* file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL){
+        if (strncmp(line, "VmSize:", 7) == 0){
+            result = parseLine(line);
+            break;
+        }
+    }
+    fclose(file);
+    return result;
+}
+
+
+static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+
+void init(){
+    FILE* file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow,
+           &lastTotalSys, &lastTotalIdle);
+    fclose(file);
+}
+
+double getCurrentValue(){
+    double percent;
+    FILE* file;
+    unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+    file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow,
+           &totalSys, &totalIdle);
+    fclose(file);
+
+    if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+        totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+        //Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else{
+        total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+                (totalSys - lastTotalSys);
+        percent = total;
+        total += (totalIdle - lastTotalIdle);
+        percent /= total;
+        percent *= 100;
+    }
+
+    lastTotalUser = totalUser;
+    lastTotalUserLow = totalUserLow;
+    lastTotalSys = totalSys;
+    lastTotalIdle = totalIdle;
+
+    return percent;
+}
+
+
+void printArray(int array[]){
+    for (int k = 0; k < range; k++) {
+        if (array[k] != 0) {
+            for (int i = 0; i < array[k]; i++) {
+                cout << k << " ";
+            }
+        }
+    }
+}
 
 void CountingSort(int arr[], int sizeOfArray, int* sortedArray) {
 
@@ -27,7 +110,9 @@ int main()
 {
     clock_t start = clock();
 
-    int a[SIZE];
+    int* a = (int *)malloc(SIZE * sizeof(int));
+    int* leftArray = (int *)malloc((SIZE/2+1) * sizeof(int));
+    int* rightArray = (int *)malloc((SIZE/2+1) * sizeof(int));
 
     for (int m = 0; m < SIZE; m++) {
         a[m] = rand()%10;
@@ -35,8 +120,6 @@ int main()
 
     int min = INT32_MAX;
     int max = -INT32_MAX;
-    int leftArray[SIZE/2+1];
-    int rightArray[SIZE/2+1];
     int leftCount = 0;
     int rightCount = 0;
 
@@ -73,15 +156,12 @@ int main()
         sortedArray[l] = leftSortedArray[l] + rightSortedArray[l];
     }
 
-    for (int k = 0; k < range; k++) {
-        if (sortedArray[k] != 0) {
-            for (int i = 0; i < sortedArray[k]; i++) {
-                cout << k << " ";
-            }
-        }
-    }
-
     clock_t stop = clock();
     double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
-    cout << "\n" << "Time: " << elapsed << "\n";
+
+//    printArray(sortedArray);
+
+    cout << "Time: " << elapsed << "s"<< endl;
+    cout << "Memory Usage: " << getValue() << "kbs" << endl;
+    cout << "CPU Usage: " << getCurrentValue() << "%"<<endl;
 }
